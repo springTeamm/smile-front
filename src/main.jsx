3cecde91af56118535ca58ccad35efa7fb0e9181
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2024-11-10 23:07:01 +0900

    feat:메인페이지에 관리자 페이지 route해서 작동되도록 만들었다
    -공통되는거여서 충돌 날수 있어서 조심해야한다

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'; 
-import Footer from './components/Footer'; 
+import Footer from './components/Footer';
+import ManagerPage from "./pages/managerpage/ManagerPage";
+import SpaceSelect from "./pages/managerpage/SpaceSelect";
+import Cancellmanagement from "./pages/managerpage/Cancellmanagement";
+import Chat from "./pages/managerpage/Chat";
+import Hostinfo from "./pages/managerpage/Hostinfo";
+import Review from "./pages/managerpage/Review";
+import Clubmanagement from "./pages/managerpage/Clubmanagement";
+import Reservationinfo from "./pages/managerpage/Reservationinfo";
+
+
 
 function Main() {
   return (
+
     <Router>
       <Header />
       <Routes>
         {/* <Route path="/" element={<MainPage />} /> */}
+
+        <Route path="ManagerPage" element={<ManagerPage />}>
+            <Route path="SpaceSelect" element={<SpaceSelect />} />
+            <Route path="cancellmanagement" element={<Cancellmanagement />} />
+            <Route path="Reservationinfo" element={<Reservationinfo />} />
+            <Route path="Chat" element={<Chat />} />
+            <Route path="Hostinfo" element={<Hostinfo/>} />
+            <Route path="clubmanagement" element={<Clubmanagement />} />
+            <Route path="Review" element={<Review />} />
+        </Route>
       </Routes>
       <Footer />
     </Router>
